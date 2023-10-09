eed666bca255b82e99ff3933fb2e04539b77c0b2
marcusSystementor <144769052+marcusSystementor@users.noreply.github.com>
2023-10-09 10:56:44 +0200

    First commit

---
+++ SimpleKey.java
@@ -0,0 +1,32 @@
+public class SimpleKey {
+
+    public static void main(String[] args) {
+
+        int key = 1;
+
+        String text = "Test";
+
+        char [] chars = text.toCharArray();
+
+        for (char c : chars) {
+            c += key;
+            System.out.print(c);
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
